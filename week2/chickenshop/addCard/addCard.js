bc0b7ed6999619c7fd979fca8c96d80e3c730b44
lydiacho <sheecho0804@ewhain.net>
2023-06-04 21:52:25 +0900

    feat : 추가 버튼 클릭 시 배열에 새 data 추가

--- week2/chickenshop/addCard/addCard.js
+++ week2/chickenshop/addCard/addCard.js
@@ -1,3 +1,5 @@
+import { menuData } from "../constant/chickenData.js";
+
 document.getElementById('menu_img').addEventListener('change',function(e) {
     let file = e.target.files[0];
 
@@ -9,5 +11,23 @@ document.getElementById('menu_img').addEventListener('change',function(e) {
     newImage.style.width = "100%";
     newImage.style.height = "100%";
     container.appendChild(newImage);
+})
+
+document.getElementById('add_btn').addEventListener('click',function(e){
+    // 메뉴 추가 버튼 클릭 시, localStorage에 저장후 메인페이지로 이동
+
+    menuData.push(
+        {
+            id:menuData.length,
+            name: document.getElementById('menu_name').value,
+            tags: document.getElementById('menu_tag').value.split(','),     // 태그 분리 
+            img : document.getElementById('img_container').firstChild.src,
+            value : document.getElementById('menu_category').value
+        }
+    )
+
+    
+
+    //window.location.href='chicken.html';
 
 })
