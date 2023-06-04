ab44accf0a472bd34515164ad3c09b9de9b50691
lydiacho <sheecho0804@ewhain.net>
2023-06-04 21:37:47 +0900

    feat : 이미지 선택 시 미리보기 기능 구현

--- week2/chickenshop/addCard/addCard.js
+++ week2/chickenshop/addCard/addCard.js
@@ -1,3 +1,13 @@
-function loadFile(input) {
-    
-}
+document.getElementById('menu_img').addEventListener('change',function(e) {
+    let file = e.target.files[0];
+
+    let container = document.getElementById('img_container');
+    container.textContent = ""
+
+    let newImage = document.createElement("img");
+    newImage.src = URL.createObjectURL(file);
+    newImage.style.width = "100%";
+    newImage.style.height = "100%";
+    container.appendChild(newImage);
+
+})
