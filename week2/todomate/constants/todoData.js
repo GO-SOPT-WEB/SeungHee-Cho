c437890847de1efc38ef086b156845b3d1ced1e1
lydiacho <sheecho0804@ewhain.net>
2023-06-09 02:17:42 +0900

    fix : todoData.js 파일 분리

---
+++ week2/todomate/constants/todoData.js
@@ -0,0 +1,32 @@
+export const TODO_DATA = [
+    {
+        title : "SCHEDULE",
+        todo : [
+            "늘어지지 말고 카공가기"
+        ]
+    },
+    {
+        title : "TODO",
+        todo : [
+            "일본여행 블로그 쓰기",
+            "오늘자 일기 쓰기"
+        ]
+    },
+    {
+        title : "SOPT",
+        todo : [
+            "솝트 투두 과제",
+            "솝트 생각 과제",
+            "자스 스터디 과제"
+        ]
+    },
+    {
+        title : "HEALTH",
+        todo : [
+            "숄더프레스",
+            "사레레",
+            "벤트오버"
+        ]
+    }
+
+];
--- week2/todomate/todo.js
+++ week2/todomate/todo.js
@@ -1,35 +1,4 @@
-const todoData = [
-    {
-        title : "SCHEDULE",
-        todo : [
-            "늘어지지 말고 카공가기"
-        ]
-    },
-    {
-        title : "TODO",
-        todo : [
-            "일본여행 블로그 쓰기",
-            "오늘자 일기 쓰기"
-        ]
-    },
-    {
-        title : "SOPT",
-        todo : [
-            "솝트 투두 과제",
-            "솝트 생각 과제",
-            "자스 스터디 과제"
-        ]
-    },
-    {
-        title : "HEALTH",
-        todo : [
-            "숄더프레스",
-            "사레레",
-            "벤트오버"
-        ]
-    }
-
-];
+import {TODO_DATA} from './constants/todoData.js';
 
 let completeCount = 0;
 
@@ -37,7 +6,7 @@ function refreshCal() {
     const selectedLeft = document.getElementById('selected_left');
     let leftCount = 0;
 
-    for (item of todoData) {
+    for (const item of TODO_DATA) {
         leftCount += item.todo.length;
     }
 
@@ -76,17 +45,17 @@ function createTodo(index, todo, todoindex) {
 
 function plusTodo(category, todo) { //category : article 태그의 id, todo : 추가하고자하는 할일 text
     const todobox = document.getElementById(category);
-    const index = category[category.length-1]; //category가 몇번째 todoData에서 몇번째 index인지 
-    todobox.appendChild(createTodo(index,todo, todoData[index-1].todo.length+1));
+    const index = category[category.length-1]; //category가 몇번째 TODO_DATA에서 몇번째 index인지 
+    todobox.appendChild(createTodo(index,todo, TODO_DATA[index-1].todo.length+1));
 
-    todoData[index-1].todo.push(todo);
+    TODO_DATA[index-1].todo.push(todo);
     refreshCal();  
 }
 
 function refreshTodo() {
-    for (let i = 1; i<=todoData.length; i++) {
-        for (let j = 0; j < todoData[i-1].todo.length; j++) {
-            const oneTodo = createTodo(i, todoData[i-1].todo[j], j+1);
+    for (let i = 1; i<=TODO_DATA.length; i++) {
+        for (let j = 0; j < TODO_DATA[i-1].todo.length; j++) {
+            const oneTodo = createTodo(i, TODO_DATA[i-1].todo[j], j+1);
             document.getElementById(`category${i}`).appendChild(oneTodo);
         }
     }
